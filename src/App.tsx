3bb7e1f611808e56d2e96523b772d1940f4a9b45
kloparn <adam97.live.com@live.com>
2020-06-08 21:46:28 +0200

    start of the navigation system

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import styled, { ThemeProvider } from "styled-components";
 import { theme, darkTheme } from "./styles/default-theme";
 import Switch from "react-switch";
@@ -12,28 +12,69 @@ function App() {
       : JSON.parse(localStorage.getItem("lightMode")!)
   );
 
-  React.useEffect(() => {
+  useEffect(() => {
     localStorage.setItem("lightMode", JSON.stringify(lightMode));
   }, [lightMode]);
 
   return (
     <ThemeProvider theme={lightMode ? theme : darkTheme}>
       <ScreenView>
-        <SwitchContainer>
-          <Switch
-            onChange={() => setMode(!lightMode)}
-            checked={!lightMode}
-          ></Switch>
-          <DarkModeParagraph>
-            <b>Dark mode</b>
-          </DarkModeParagraph>
-        </SwitchContainer>
+        <Container>
+          <Navbar>
+            <NavbarItem>
+              <ItemAnchor href="#"> home </ItemAnchor>
+            </NavbarItem>
+            <NavbarItem>
+              <ItemAnchor href="#"> Projects </ItemAnchor>
+            </NavbarItem>
+            <NavbarItem>
+              <ItemAnchor href="#"> About me </ItemAnchor>
+            </NavbarItem>
+            <SwitchContainer>
+              <Switch
+                onChange={() => setMode(!lightMode)}
+                checked={!lightMode}
+              ></Switch>
+              <DarkModeParagraph>
+                <b>Dark mode</b>
+              </DarkModeParagraph>
+            </SwitchContainer>
+          </Navbar>
+        </Container>
         {/* Navbar shall go here with the {home(theme) going inside it}*/}
         {home(theme)}
       </ScreenView>
     </ThemeProvider>
   );
 }
+
+const Container = styled.div`
+  display: center;
+  padding: 5px;
+`;
+
+const Navbar = styled.ul`
+  width: 100%;
+  margin: 0 0 3em 0;
+  padding: 0;
+  list-style: none;
+`;
+const NavbarItem = styled.li`
+  float: left;
+`;
+
+const ItemAnchor = styled.a`
+  display: block;
+  padding: 8px 15px;
+  text-decoration: none;
+  font-weight: bold;
+  color: ${(props) => props.theme.colors.text};
+  border-right: 1px solid #ccc;
+  :hover {
+    color: "#00ff00";
+    cursor: pointer;
+  }
+`;
 
 const ScreenView = styled.div`
   position: absolute;
@@ -47,6 +88,7 @@ const SwitchContainer = styled.div`
   float: right;
   color: ${(props) => props.theme.colors.text};
   display: table-row;
+  padding: 10px;
 `;
 const DarkModeParagraph = styled.p`
   font-display: bold;
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React from "react";
 import { DefaultTheme } from "styled-components";
-import styled, { ThemeProvider } from "styled-components";
+import styled from "styled-components";
 
 export const home = (props: DefaultTheme) => {
   return (
